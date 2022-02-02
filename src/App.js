2ba0113c019c94b757c5daf4f7152504bc4f18f8
Salim ZIDI <salimzidi09@gmail.com>
2022-02-02 10:30:52 +0100

    JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './style.css'
+import xyz from "./dog.jpg"
+// video src
+// import sz from "./sss.mp4"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='blocc'>
+      <div style={{border:"solid 1px black", maxWidth:"100vw"}}>
+
+      <h1 className="titleRed"> Salim </h1>
+
+       <br />
+    {/* src img */}
+
+      <img className='foto' src={xyz} alt="dogg"/>
+
+    <br />
+    {/* public img */}
+
+<img src="/salim.jpg" alt="salim"/>
+
+     </div>
+     {/* public video */}
+
+<video width="320" height="240" controls>
+
+    <source src="/sss.mp4" type="video/mp4" />
+
+   </video>
+   {/* video dans src!!! */}
+   {/* <video width="320" height="240" controls>
+
+    <source src={sz} type="video/mp4" />
+
+   </video> */}
+</div>
   );
 }
 
 export default App;
+     
